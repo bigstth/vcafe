7668431a4e99eda1c0fd6a36bae606a79b99c728
Bigstth <sitthidsn@gmail.com>
2025-07-01 01:49:33 +0700

    🔧 Remove debug log from auth middleware

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -4,7 +4,6 @@ import { HTTPException } from 'hono/http-exception'
 
 export const protect = createMiddleware(async (c, next) => {
     const session = await auth.api.getSession(c.req.raw)
-    console.log('Session:', session)
     if (!session?.user) {
         throw new HTTPException(401, { message: 'Unauthorized' })
     }
