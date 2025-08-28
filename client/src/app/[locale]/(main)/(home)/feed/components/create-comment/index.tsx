655689544e8bbaf4213d7403131b16672d95af36
Bigstth <sitthidsn@gmail.com>
2025-08-28 23:57:34 +0700

    💄 Replaced the Card with a Dialog component

--- client/src/app/[locale]/(main)/(home)/feed/components/create-comment/index.tsx
+++ client/src/app/[locale]/(main)/(home)/feed/components/create-comment/index.tsx
@@ -1,20 +1,20 @@
-import { Card, CardContent } from '@/components/ui/card'
 import { useForm } from 'react-hook-form'
 import { useCreateComment } from './use-create-comment'
 import { CommentSchemaType, type Post } from '../../types'
 import { toast } from 'sonner'
 import debounce from 'lodash/debounce'
 import CreateCommentForm from './form'
-import { X } from 'lucide-react'
 import { renderImages } from '@/components/post-card/image-render'
 import { useTimeAgo } from '@/hooks/use-get-time-ago'
 import AvatarRole from '@/components/avatar-with-role-border'
 import { Link } from '@/i18n/navigation'
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog'
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
 
 type CreateCommentProps = {
     post: Post | null
     showCreateComment: boolean
-    setShowCreateComment?: React.Dispatch<React.SetStateAction<boolean>>
+    setShowCreateComment: React.Dispatch<React.SetStateAction<boolean>>
 }
 const CreateComment: React.FC<CreateCommentProps> = ({
     post,
@@ -45,57 +45,52 @@ const CreateComment: React.FC<CreateCommentProps> = ({
     }, 500)
 
     return (
-        <div className={` ${showCreateComment ? 'block' : 'hidden'}`}>
-            <div className="fixed inset-0 z-40 bg-black/40 backdrop-blur-xs" />
-            <div className="z-50 fixed w-[600px] top-1/8 left-1/2 -translate-x-1/2">
-                <Card>
-                    <CardContent>
-                        <button onClick={() => setShowCreateComment?.(false)}>
-                            <X className="absolute top-4 right-4 cursor-pointer text-xl mx-4 my-2" />
-                        </button>
-
-                        <div className="flex items-start gap-4 w-full">
-                            <AvatarRole
-                                image={post?.author?.image}
-                                username={post?.author?.username}
-                                role={post?.author?.role}
-                            />
-                            <div className="flex-1 flex flex-col gap-4 min-w-0">
-                                <div>
-                                    <Link href={`/${post?.author.username}`}>
-                                        <span className="font-semibold">
-                                            {post?.author.displayUsername ||
-                                                post?.author.username}
-                                        </span>
-                                        <span className="text-foreground/50 font-medium">
-                                            {` @${post?.author.username}`}
-                                        </span>
-                                    </Link>
-                                    <Link
-                                        href={`${post?.author.username}/posts/${post?.id}`}
-                                    >
-                                        <span className="text-foreground/50 font-medium">
-                                            {` `}·{' '}
-                                            {post?.createdAt
-                                                ? getTimeAgo(
-                                                      new Date(post.createdAt)
-                                                  )
-                                                : ''}
-                                        </span>
-                                    </Link>
-                                </div>
-                                <pre className="whitespace-pre-wrap break-words">
-                                    {post?.content}
-                                </pre>
-                                {post && renderImages(post)}
-                            </div>
+        <Dialog
+            open={showCreateComment}
+            onOpenChange={() => setShowCreateComment(false)}
+        >
+            <DialogContent>
+                <VisuallyHidden>
+                    <DialogTitle>Reply a post.</DialogTitle>
+                </VisuallyHidden>
+                <div className="flex items-start gap-4 w-full">
+                    <AvatarRole
+                        image={post?.author?.image}
+                        username={post?.author?.username}
+                        role={post?.author?.role}
+                    />
+                    <div className="flex-1 flex flex-col gap-4 min-w-0">
+                        <div>
+                            <Link href={`/${post?.author.username}`}>
+                                <span className="font-semibold">
+                                    {post?.author.displayUsername ||
+                                        post?.author.username}
+                                </span>
+                                <span className="text-foreground/50 font-medium">
+                                    {` @${post?.author.username}`}
+                                </span>
+                            </Link>
+                            <Link
+                                href={`${post?.author.username}/posts/${post?.id}`}
+                            >
+                                <span className="text-foreground/50 font-medium">
+                                    {` `}·{' '}
+                                    {post?.createdAt
+                                        ? getTimeAgo(new Date(post.createdAt))
+                                        : ''}
+                                </span>
+                            </Link>
                         </div>
+                        <pre className="whitespace-pre-wrap break-words">
+                            {post?.content}
+                        </pre>
+                        {post && renderImages(post)}
+                    </div>
+                </div>
 
-                        <CreateCommentForm onSubmit={onSubmit} />
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
+                <CreateCommentForm onSubmit={onSubmit} />
+            </DialogContent>
+        </Dialog>
     )
 }
 
